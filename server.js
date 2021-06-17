60ec41772c0db50a28848738dde01242882ee811
ChrisMwanya <cmwanya@gmail.com>
2021-06-17 15:07:49 +0100

    feat : add server file

---
+++ server.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-undef */
+const express = require('express')
+const app = express();
+const connectDB = require('./config/db')
+
+app.use((req, res,next) => {
+    req.con= connectDB;
+    next()
+})
+
+app.use(express.json())
+app.use(cors());
+
+const technoRouter = require('./routes/technoroute');
+
+app.use("/techno",technoRouter)
+
+app.listen(3009,()=>{
+    console.log("server listening on 3009")
+})
