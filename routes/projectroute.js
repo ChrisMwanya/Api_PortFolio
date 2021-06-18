625ae24741c99010214fb2c7b8897e262795a862
ChrisMwanya <cmwanya@gmail.com>
2021-06-18 15:04:21 +0100

    feat: add another routes

--- routes/projectroute.js
+++ routes/projectroute.js
@@ -2,13 +2,16 @@
 const express = require("express");
 const router = express.Router();
 const projectController = require("./../controllers/projectcontrollers");
-const projectTechnoController =require("./../controllers/projecttechnocontroller")
+const projectTechnoController = require("./../controllers/projecttechnocontroller");
 
 const BASE_URI = "/api_portfolio";
 
 router.get(`${BASE_URI}/projects`, projectController.getProjects);
 router.get(`${BASE_URI}/project/:id`, projectController.getProject);
-router.get(`${BASE_URI}/project/:id/technos`, projectTechnoController.getTechnoByProject);
+router.get(
+  `${BASE_URI}/project/:id/technos`,
+  projectTechnoController.getTechnoByProject
+);
 router.post(`${BASE_URI}/project/add`, projectController.addProject);
 router.put(`${BASE_URI}/project/edit/:id`, projectController.updateProject);
 router.delete(
--- routes/technoroute.js
+++ routes/technoroute.js
@@ -2,13 +2,16 @@
 const express = require("express");
 const router = express.Router();
 const technoController = require("./../controllers/technocontroller");
-const projectTechnoController = require("./../controllers/projecttechnocontroller")
+const projectTechnoController = require("./../controllers/projecttechnocontroller");
 
 const BASE_URI = "/api_portfolio";
 
 router.get(`${BASE_URI}/technos`, technoController.getTechnos);
 router.get(`${BASE_URI}/techno/:id`, technoController.getTechno);
-router.get(`${BASE_URI}/techno/:id/projects`, projectTechnoController.getProjectByTechno);
+router.get(
+  `${BASE_URI}/techno/:id/projects`,
+  projectTechnoController.getProjectByTechno
+);
 router.post(`${BASE_URI}/techno/add`, technoController.addTechno);
 router.put(`${BASE_URI}/techno/edit/:id`, technoController.updateTechno);
 router.delete(`${BASE_URI}/techno/delete/:id`, technoController.deleteTechno);
