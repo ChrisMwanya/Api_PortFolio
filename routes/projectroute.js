fe35aaf5a5db5ff200e9af0de00bd6de1a313c1e
ChrisMwanya <cmwanya@gmail.com>
2021-06-18 13:03:47 +0100

    feat : add project route

---
+++ routes/projectroute.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-undef */
+const express = require("express");
+const router = express.Router();
+const projectController = require("./../controllers/projectcontrollers");
+
+const BASE_URI = "/api_portfolio";
+
+router.get(`${BASE_URI}/projects`, projectController.getProjects);
+router.get(`${BASE_URI}/project/:id`, projectController.getProject);
+router.post(`${BASE_URI}/project/add`, projectController.addProject);
+router.put(`${BASE_URI}/project/edit/:id`, projectController.updateProject);
+router.delete(`${BASE_URI}/project/delete/:id`, projectController.deleteProject);
+
+module.exports = router;
