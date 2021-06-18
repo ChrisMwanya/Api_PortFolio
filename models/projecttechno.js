9230fc6a08d4bc1e8e20aab86d2412d472418d4b
ChrisMwanya <cmwanya@gmail.com>
2021-06-18 14:31:28 +0100

    feat: add the method retrieving all the technos of a project

--- models/projecttechno.js
+++ models/projecttechno.js
@@ -1,57 +1,57 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-	get: (connection, callback) => {
-		connection.query("SELECT * FROM project_techno", callback);
-	},
+  get: (connection, callback) => {
+    connection.query("SELECT * FROM project_techno", callback);
+  },
 
-	getById: (connection, idProjectTechno, callback) => {
-		connection.query(
-			"SELECT * FROM project_techno WHERE id_project_techno  = ?",
-			idProjectTechno,
-			callback
-		);
-	},
+  getById: (connection, idProjectTechno, callback) => {
+    connection.query(
+      "SELECT * FROM project_techno WHERE id_project_techno  = ?",
+      idProjectTechno,
+      callback
+    );
+  },
 
-	getByIdProject: (connection, idProject, callback) => {
-		connection.query(
-			"SELECT * FROM techno INNER JOIN project_techno ON project_techno.id_techno=techno.id_techno WHERE project_techno.id_project = ?",
-			idProject,
-			callback
-		);
-	},
+  getByIdProject: (connection, idProject, callback) => {
+    connection.query(
+      "SELECT * FROM techno INNER JOIN project_techno ON project_techno.id_techno=techno.id_techno WHERE project_techno.id_project = ?",
+      idProject,
+      callback
+    );
+  },
 
-	getByIdTechno: (connection, idTechno, callback) => {
-		connection.query(
-			"SELECT * FROM project INNER JOIN project_Techno ON project.id_project = project_techno.id_project WHERE project_techno.id_techno = ?",
-			idTechno,
-			callback
-		);
-	},
+  getByIdTechno: (connection, idTechno, callback) => {
+    connection.query(
+      "SELECT * FROM project INNER JOIN project_Techno ON project.id_project = project_techno.id_project WHERE project_techno.id_techno = ?",
+      idTechno,
+      callback
+    );
+  },
 
-	create: (connection, projectTechnoToInsert, callback) => {
-		connection.query(
-			"INSERT INTO project_techno(id_techno,id_project ) VALUES (?,?)",
-			[projectTechnoToInsert.idTechno, projectTechnoToInsert.idProject],
-			callback
-		);
-	},
-	update: (connection, projectTechnoToUpdate, callback) => {
-		connection.query(
-			"UPDATE project_techno SET id_techno = ?, id_project = ? WHERE id_project_techno = ?",
-			[
-				projectTechnoToUpdate.idTechno,
-				projectTechnoToUpdate.idProject,
-				projectTechnoToUpdate.id,
-			],
-			callback
-		);
-	},
+  create: (connection, projectTechnoToInsert, callback) => {
+    connection.query(
+      "INSERT INTO project_techno(id_techno,id_project ) VALUES (?,?)",
+      [projectTechnoToInsert.idTechno, projectTechnoToInsert.idProject],
+      callback
+    );
+  },
+  update: (connection, projectTechnoToUpdate, callback) => {
+    connection.query(
+      "UPDATE project_techno SET id_techno = ?, id_project = ? WHERE id_project_techno = ?",
+      [
+        projectTechnoToUpdate.idTechno,
+        projectTechnoToUpdate.idProject,
+        projectTechnoToUpdate.id,
+      ],
+      callback
+    );
+  },
 
-	delete: (connection, idProjectTechno, callback) => {
-		connection.query(
-			"DELETE FROM project_techno WHERE id_project_techno = ?",
-			idProjectTechno,
-			callback
-		);
-	},
+  delete: (connection, idProjectTechno, callback) => {
+    connection.query(
+      "DELETE FROM project_techno WHERE id_project_techno = ?",
+      idProjectTechno,
+      callback
+    );
+  },
 };
